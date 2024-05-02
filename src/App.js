71cfa83352ec966122130a97f4169caf626c0684
Ribeirovisk <franckrm2001@gmail.com>
2024-05-02 18:09:12 -0300

    wip: finishing logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
 import './App.css'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = ()=> {
-    const message = "Hello world"
+    const [tasks, setTasks] = useState([
+        {
+            id: uuidv4(),
+            title: 'Estudar Programação',
+            completed:false,
+        
+        },
+        {
+            id: uuidv4(),
+            title: 'Ler Livros',
+            completed:false,
+        
+        }
+        
+    ])
+
+    const handleTaskClick = (taskId) =>{
+        const newTasks = tasks.map(task=>{
+            if (task.id === taskId) return {...task, completed: !task.completed}
+            return task
+        })
+        setTasks(newTasks)
+    }
+
+    const handleTaskAddition = (taskTitle)=>{
+        const newTasks = [
+            ...tasks, 
+            {
+                title: taskTitle,
+                id: uuidv4(),
+                completed: false
+            }
+        ]   
+
+        setTasks(newTasks);
+    }
+   
     return (
-        <div className="container">{message}</div>
+        <>
+            <div className="container">
+                <AddTask handleTaskAddition={handleTaskAddition}/>
+                <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
+            </div>
+        </>
     )
 }
 
