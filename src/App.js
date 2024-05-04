624e7d26f9183736e4b8d4a19afbb176928d1670
Ribeirovisk <franckrm2001@gmail.com>
2024-05-04 16:51:03 -0300

    wip: finish base project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import axios from 'axios'
 
-import './App.css'
+
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Header from './components/Header'
+import TaskDetails from './components/TaskDetails'
+
+import './App.css'
+import { SiAnsys } from 'react-icons/si'
 
 const App = ()=> {
     const [tasks, setTasks] = useState([
@@ -18,9 +25,31 @@ const App = ()=> {
             title: 'Ler Livros',
             completed:false,
         
+        },
+        {
+            id: uuidv4(),
+            title: 'Teste 1',
+            completed:false,
+        
+        },
+        {
+            id: uuidv4(),
+            title: 'Teste 2',
+            completed:false,
+        
         }
         
     ])
+
+    useEffect(()=>{
+        const feachTasks = async ()=>{
+            const {data} = await  axios.get('https://jsonplaceholder.cypress.io/todos?_limit=10')
+            setTasks(data);
+        }
+       
+        feachTasks();
+        
+    }, [])
 
     const handleTaskClick = (taskId) =>{
         const newTasks = tasks.map(task=>{
@@ -29,7 +58,7 @@ const App = ()=> {
         })
         setTasks(newTasks)
     }
-
+    
     const handleTaskAddition = (taskTitle)=>{
         const newTasks = [
             ...tasks, 
@@ -42,14 +71,39 @@ const App = ()=> {
 
         setTasks(newTasks);
     }
+
+    const handleTaskDeletion = (taskId)=>{
+        const newTasks = tasks.filter((task)=> task.id !== taskId);
+        setTasks(newTasks);
+    }
    
     return (
-        <>
+        <Router>
             <div className="container">
-                <AddTask handleTaskAddition={handleTaskAddition}/>
-                <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
+                <Header/>
+                <Routes>
+                    <Route 
+                        path="/" 
+                        element={
+                            <>
+                                <AddTask handleTaskAddition={handleTaskAddition}/>
+                                <Tasks tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
+                            </>
+                        }
+                    /> 
+                    <Route 
+                        path="/:taskTitle" 
+                        element={
+                            <>
+                               <TaskDetails/>
+                            </>
+                        }
+                    /> 
+              
+
+                </Routes> 
             </div>
-        </>
+        </Router>
     )
 }
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,9 @@
 import Task from "./Task";
-
-const Tasks = ({tasks, handleTaskClick})=>{
+const Tasks = ({tasks, handleTaskClick, handleTaskDeletion})=>{
     return(
         <>
             {tasks.map(task=>(
-                <Task task={task} handleTaskClick={handleTaskClick}/>
+                <Task task={task} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
             ))}
         </>
     )
